39ce210453c57dcc34b13f176da92851dc19411c
Silviyavd <dineva.sv@gmail.com>
2022-10-28 07:19:48 +0300

    add login page

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,5 +1,28 @@
+import './Login.css';
+
 export const Login = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.password.value);
+    }
+
     return (
-        <h1>Login Page</h1>
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
+                <div class="input-group">
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" name="username" placeholder="John Doe"/>                    
+                </div>
+                <div class="input-group">
+                    <label htmlFor="password">Password:   </label>
+                    <input type="text" name="password"/>              
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
     )
 };
